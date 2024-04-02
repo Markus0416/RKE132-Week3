7c3ee0220f38c71b0707c7820de21825ae8f8ed0
Markus <markus.mursal@tktk.ee>
2024-04-02 14:52:59 +0300

    add Week3IfAndSwitch code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if (weekDay == 0)
+{
+    Console.WriteLine("it´s Sunday!");
+}
+else  if (weekDay == 1)
+{
+    Console.WriteLine("it´s Monday!");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("it´s Tuesday!");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("it´s Wednesday!");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("it´s Thursday!");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("it´s Friday!");
+}
+else if (weekDay == 6)
+{
+    Console.WriteLine("it´s Satueday!");
+}
+
+Console.WriteLine("Have a nice day!");
